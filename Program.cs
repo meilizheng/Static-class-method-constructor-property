9bb020e9b3661203849dada9f6b0324eae0ab6c9
meilizheng <116130097+meilizheng@users.noreply.github.com>
2023-05-04 14:14:02 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Static_class_method_constructor_property
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Static-class-method-constructor-property
+ 
